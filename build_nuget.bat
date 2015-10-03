164367a1d6bac0fd643835077de675cc03138d25
Ivan Gagis <igagis@gmail.com>
2015-10-03 18:12:21 +0300

    stuff

--- build_nuget.bat
+++ build_nuget.bat
@@ -7,3 +7,5 @@ msbuild msvs_solution/msvs_solution.sln /property:Configuration=Debug /property:
 msbuild msvs_solution/msvs_solution.sln /property:Configuration=Debug /property:Platform=x64
 msbuild msvs_solution/msvs_solution.sln /property:Configuration=Release /property:Platform=x86
 msbuild msvs_solution/msvs_solution.sln /property:Configuration=Release /property:Platform=x64
+
+powershell Write-NuGetPackage nuget.autopkg
