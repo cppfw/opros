4a1196c7e7091e473fb88e3a7ec6308f9d1b5a47
Ivan Gagis <igagis@gmail.com>
2021-04-09 09:43:33 +0300

    add configs

--- tests/test_target.mk
+++ tests/test_target.mk
@@ -1,9 +1,9 @@
 ifeq ($(os),windows)
-    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src/build;%PATH% && $$(notdir $$^)')
+    this_test_cmd := (cd $(d) && cmd //C 'set PATH=../../src/out/$(c);%PATH% && out\$(c)\$$(notdir $$^)')
 else ifeq ($(os),macosx)
-    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/build ./$$(notdir $$^))
+    this_test_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c) out/$(c)/$$(notdir $$^))
 else ifeq ($(os),linux)
-    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/build ./$$(notdir $$^))
+    this_test_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/out/$(c) out/$(c)/$$(notdir $$^))
 else
     $(error "Unknown OS")
 endif
@@ -14,24 +14,21 @@ this_test := $(word $(words $(this_dirs)), $(this_dirs))
 define this_rule
 test:: $(prorab_this_name)
 $(.RECIPEPREFIX)@myci-running-test.sh $(this_test)
-$(.RECIPEPREFIX)$(Q)$(this_test_cmd) || myci-error.sh "test failed"
+$(.RECIPEPREFIX)$(a)$(this_test_cmd) || myci-error.sh "test failed"
 $(.RECIPEPREFIX)@myci-passed.sh
 endef
 $(eval $(this_rule))
 
-
-
 ifeq ($(os),windows)
-    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src/build;%PATH% && gdb $$(notdir $$^)')
+    this_gdb_cmd := (cd $(d) && cmd //C 'set PATH=../../src/out/$(c);%PATH% && gdb out\$(c)\$$(notdir $$^)')
 else ifeq ($(os),macosx)
-    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/build gdb ./$$(notdir $$^))
+    this_gdb_cmd := (cd $(d) && DYLD_LIBRARY_PATH=../../src/out/$(c) gdb out/$(c)/$$(notdir $$^))
 else ifeq ($(os),linux)
-    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/build gdb ./$$(notdir $$^))
+    this_gdb_cmd := (cd $(d) && LD_LIBRARY_PATH=../../src/out/$(c) gdb out/$(c)/$$(notdir $$^))
 endif
-
 
 define this_rule
 gdb:: $(prorab_this_name)
-$(.RECIPEPREFIX)$(Q)$(this_gdb_cmd)
+$(.RECIPEPREFIX)$(a)$(this_gdb_cmd)
 endef
 $(eval $(this_rule))
