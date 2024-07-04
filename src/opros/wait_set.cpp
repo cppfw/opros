a54645813a8419a213555b79c575a559638c6c48
Ivan Gagis <igagis@gmail.com>
2024-07-05 00:09:49 +0300

    format

--- src/opros/wait_set.cpp
+++ src/opros/wait_set.cpp
@@ -101,9 +101,8 @@ wait_set::wait_set(unsigned capacity) :
 
 void wait_set::add_filter(waitable& w, int16_t filter, void* user_data)
 {
-	struct kevent e
-		// WORKAROUND: clang-format is unstable on this line
-		{};
+	using kevent_struct = struct kevent;
+	kevent_struct e{};
 
 	EV_SET(&e, w.handle, filter, EV_ADD | EV_RECEIPT, 0, 0, user_data);
 
@@ -128,9 +127,8 @@ void wait_set::add_filter(waitable& w, int16_t filter, void* user_data)
 
 void wait_set::remove_filter(waitable& w, int16_t filter) noexcept
 {
-	struct kevent e
-		// WORKAROUND: clang-format is unstable on this line
-		{};
+	using kevent_struct = struct kevent;
+	kevent_struct e{};
 
 	EV_SET(&e, w.handle, filter, EV_DELETE | EV_RECEIPT, 0, 0, nullptr);
 
